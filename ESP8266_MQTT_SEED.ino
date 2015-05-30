5fe3e299ec1e0adf10d977c56bebcb0dc35e9cb0
Nat Weerawan <nat.wrw@gmail.com>
2015-05-30 14:07:53 +0700

    Add preprocessor directive to support DEBUG or PRODUCTION MODE.

--- ESP8266_MQTT_SEED.ino
+++ ESP8266_MQTT_SEED.ino
@@ -4,7 +4,18 @@
 #define WIFI_MAX_RETRIES 150
 #define WIFI_CONNECT_DELAY_MS 100
 
+// #define DEBUG_MODE
+
 #define CLIENT_ID_PREFIX "esp8266-"
+
+#define STATE_WIFI_CONNECTING   0
+#define STATE_WIFI_CONNECTED    1
+#define STATE_MQTT_CONNECTED    2
+#define STATE_MQTT_SUBSCRIBED   3
+#define STATE_READY_TO_GO       4
+
+
+#define LED_PIN 1 // <<<==== 1 = TX0 PIN 
 
 const char *ssid = "OpenWrt_NAT_500GP.101";
 const char *pass = "activegateway";
@@ -42,22 +53,66 @@ String macToStr(char* target)
 }
 
 
+void blink_ms(uint8_t millisecs) {
+  digitalWrite(LED_PIN, HIGH);
+  delay(millisecs);
+  digitalWrite(LED_PIN, LOW);  
+  delay(millisecs);
+}
+
+void visualNotify(uint8_t state) {
+
+
+    if (state == STATE_WIFI_CONNECTING) {
+      #ifdef DEBUG_MODE      
+        Serial.print(".");
+      #else
+        blink_ms(30);
+      #endif  
+    }
+    else if (state == STATE_MQTT_CONNECTED) {
+        blink_ms(30);
+    }
+    else if (state == STATE_MQTT_SUBSCRIBED) {
+        blink_ms(30);
+    }
+    else if (state == STATE_WIFI_CONNECTED) {
+      #ifdef DEBUG_MODE      
+        Serial.print("Wifi connected.");
+      #else
+        blink_ms(100);
+        delay(50);
+        blink_ms(100);
+        delay(50);
+        blink_ms(100);
+      #endif  
+    }
+  
+  
+}
 
 void setup()
 {
-  // Setup console
-  Serial.begin(115200);
-  delay(10);
-  Serial.println();
-  Serial.println();
+
 
   client.set_callback(callback);
+
+  #ifdef DEBUG_MODE
+    // Setup console
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println();  
+  #else
+    pinMode(LED_PIN, OUTPUT);
+  #endif
+  
+  delay(10);
 
   WiFi.begin(ssid, pass);
   
   int retries = 0;
   while ((WiFi.status() != WL_CONNECTED)) {
-    Serial.print(".");   
+    visualNotify(STATE_WIFI_CONNECTING);    
     if(retries > WIFI_MAX_RETRIES){
       Serial.println("Reset due to WIFI_MAX_RETRIES");
       abort();
@@ -66,7 +121,9 @@ void setup()
     delay(WIFI_CONNECT_DELAY_MS); 
   }
 
-  Serial.println("\n WiFi connected");
+
+  visualNotify(STATE_WIFI_CONNECTED);
+
 
   macToStr(clientId);
 
